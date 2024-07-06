9e998d207608d98477dfcf9e76e3066209ba21b1
astridcoy <as.coy@duocuc.cl>
2024-07-06 01:17:30 -0400

    act

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import django.core.management
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tienda.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TiendaWeb.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
