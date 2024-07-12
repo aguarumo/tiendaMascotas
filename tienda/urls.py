d0be5db77ec923e5f1af75bf4bfefe27faf76418
Andres <an.guarumo@duocuc.cl>
2024-07-12 13:06:19 -0400

    correccion imagenes

--- tienda/urls.py
+++ tienda/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -23,5 +25,5 @@ urlpatterns = [
     path('formulario', views.formulario, name='formulario'),
     path('donaciones', views.donaciones, name='donaciones'),
     path('contacto', views.contacto, name='contacto'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
  
